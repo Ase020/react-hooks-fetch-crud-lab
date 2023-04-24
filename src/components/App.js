86bd4878cf70a2b7838ae895c8a12a76b75ebde2
felixNyalenda <felixasejunior@gmail.com>
2023-04-24 13:30:33 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+   const [page, setPage] = useState("List");
+   const [questions, setQuestions] = useState([]);
 
-  return (
-    <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
-    </main>
-  );
+   useEffect(() => {
+      fetch("http://localhost:4000/questions")
+         .then((res) => res.json())
+         .then((data) => {
+            setQuestions(data);
+         });
+   }, [questions]);
+
+   return (
+      <main>
+         <AdminNavBar onChangePage={setPage} />
+         {page === "Form" ? <QuestionForm setQuestions={setQuestions} /> : <QuestionList questions={questions} />}
+      </main>
+   );
 }
 
 export default App;
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,53 @@
 import React from "react";
 
 function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+   const options = answers.map((answer, index) => (
+      <option key={index} value={index}>
+         {answer}
+      </option>
+   ));
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
+   const handleDelete = (id) => {
+      fetch(`http://localhost:4000/questions/${id}`, {
+         method: "DELETE",
+         headers: {
+            "Content-Type": "application/json",
+         },
+      });
+   };
+
+   const handleChange = (id, e) => {
+      fetch(`http://localhost:4000/questions/${id}`, {
+         method: "PATCH",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            correctIndex: parseInt(e.target.value),
+         }),
+      });
+   };
+
+   return (
+      <li>
+         <h4>Question {id}</h4>
+         <h5>Prompt: {prompt}</h5>
+         <label>
+            Correct Answer:
+            <select
+               defaultValue={correctIndex}
+               onChange={(e) => {
+                  handleChange(id, e);
+               }}
+            >
+               {options}
+            </select>
+         </label>
+         <button onClick={() => handleDelete(id)}>Delete Question</button>
+      </li>
+   );
 }
 
 export default QuestionItem;
